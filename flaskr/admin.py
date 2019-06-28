76557820c66ec5e5c3a7e68b7fa8fc5b2addd5b4
Lars Erik Grambo <0kvakk@gmail.com>
2019-06-28 10:15:52 +0200

    Add placeholder for creating categories
    Also moved the fetching of tag & category data to a get function that can be called from anywhere.
    Replaced the placeholder functions with flash messages.

--- flaskr/admin.py
+++ flaskr/admin.py
@@ -11,17 +11,27 @@ bp = Blueprint('admin', __name__, url_prefix='/admin')
 
 @bp.route('/killcat/<cat>')
 def killcat(cat):
-    return(f"Wery well sir, killing the cat {cat}")
+    flash(f"Wery well sir, killing the cat {cat}")
+    return render_template('blog/admin.html', **GetAdminpageData())
 @bp.route('/killtag/<tag>')
 def killtag(tag):
-    return(f"Wery well sir, eliminating the tag {tag}")
+    flash(f"Wery well sir, eliminating the tag {tag}")
+    return render_template('blog/admin.html', **GetAdminpageData())
 @bp.route('/colortag/<tag>')
 def colortag(tag):
-    return(f"Crayons drawn and ready! How shall we color {tag}?")
-
+    flash(f"Crayons drawn and ready! How shall we color {tag}?")
+    return render_template('blog/admin.html', **GetAdminpageData())
+@bp.route('/makecat', methods=("POST",))
+def makecat():
+    catname:str = request.form['catname']
+    flash(f"Wery well sir, making the cat {catname}")
+    return render_template('blog/admin.html', **GetAdminpageData())
 
 @bp.route('/')
 def adminmainpage():
+    return render_template('blog/admin.html', **GetAdminpageData())
+
+def GetAdminpageData():
     tags = {}       #all unique tags
     tagcounts = {} # number of posts by tag
     tagcolors = {} # designated color of each tag (default to 'Not set')
@@ -44,5 +54,4 @@ def adminmainpage():
                 (tag_text,)
             ).fetchall())
         tagcolors[tag_text] = "NOT SET" #TODO: implement tag colors
-
-    return render_template('blog/admin.html', categories = categories, tags = tags, tagcounts = tagcounts, tagcolors = tagcolors)
+    return {"categories" : categories, "tags" : tags, "tagcounts" : tagcounts, "tagcolors" : tagcolors}
